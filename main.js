95400043117201e88e987a24885840706289c12a
muhammadmasud1 <mdmasudrana15020@gmail.com>
2023-08-15 18:31:14 +0600

    travel

---
+++ main.js
@@ -0,0 +1,7 @@
+let nav = document.getElementById('navbar');
+let ShowNav = document.getElementById('ShowNav');
+
+
+ShowNav.addEventListener('click', function(){
+            nav.classList.toggle('show-toggle');
+})
